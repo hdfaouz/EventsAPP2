5f6d315047c3c786c5563e73b96da4ae27357e64
hdfaouz <hdfaouzia2@gmail.com>
2025-06-05 22:35:39 +0100

    navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {RegisterComponent} from "./register/register.component";
 import {LoginComponent} from "./login/login.component";
 import {AddEventComponent} from "./add-event/add-event.component";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 
 export const routes: Routes = [
@@ -9,6 +10,7 @@ export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'add-event', component: AddEventComponent },
-  { path: 'edit-event/:id', component:AddEventComponent}
+  { path: 'edit-event/:id', component:AddEventComponent},
+  {path : 'navbar' , component : NavbarComponent}
   ]
 
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,29 +7,13 @@ import {Router} from "@angular/router";
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    MatIcon,
-    MatToolbarRow,
-    MatToolbar
+
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
-  constructor( private router : Router) {
-  }
-  navigateToEvents() {
-    this.router.navigate(['/events']);
-  }
 
-  navigateToReservations() {
-    this.router.navigate(['/reservations']);
-  }
-
-  logout() {
-    // Logique de déconnexion
-    localStorage.removeItem('token'); // Exemple
-    this.router.navigate(['/login']);
-  }
 
 }
